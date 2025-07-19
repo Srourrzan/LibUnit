402eea76ef92123a3ed0b7377cd8cd575335b2f3
Razan Srour <razanmsrour@gmail.com>
2025-07-19 19:50:15 +0300

    start bzero

---
+++ tests/ft_bzero/00_launcher.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   00_launcher.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/19 17:42:21 by rsrour            #+#    #+#             */
+/*   Updated: 2025/07/19 19:47:07 by rsrour           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../framework/libunit.h"
+#include "../tests.h"
+
+int ft_strlen_launcher(void)
+{
+    t_list	*test_list;
+    int result;
+
+    test_list = NULL;
+    load_test(&test_list, "ft_strlen OK test", &ft_strlen_ok_test);
+    load_test(&test_list, "ft_strlen KO test", &ft_strlen_ko_test);
+    load_test(&test_list, "ft_strlen SEGSEGV test", &ft_strlen_segv_test);
+    load_test(&test_list, "ft_strlen BUS test", &ft_strlen_segbus_test);
+
+    result = launch_tests("ft_strlen", &test_list);
+    return (result);
+}
